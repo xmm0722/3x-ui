1baf6d74d02ed0fe57c90d8e6ebe4a4f3830eb6e
xmm0722 <312869794@qq.com>
2026-03-14 19:23:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,293 +1,161 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e
+
+REPO="xmm0722/3x-ui"
+VERSION="v2.5.0"
+INSTALL_DIR="/usr/local/x-ui"
 
 red='\033[0;31m'
 green='\033[0;32m'
-blue='\033[0;34m'
 yellow='\033[0;33m'
 plain='\033[0m'
 
-cur_dir=$(pwd)
+echo -e "${green}3x-ui Installer (fork version)${plain}"
+echo -e "Repo: ${REPO}"
+echo -e "Version: ${VERSION}"
+echo ""
 
-# 检查 root 权限
-[[ $EUID -ne 0 ]] && echo -e "${red}致命错误: ${plain} 请使用 root 权限运行此脚本 \n " && exit 1
-
-if ! ip addr show lo | grep -q '127.0.0.1'; then
-    echo "127.0.0.1 not found, adding it to loopback interface..."
-    sudo ip addr add 127.0.0.1/8 dev lo
-else
-    echo "127.0.0.1 is already configured."
+# root check
+if [[ $EUID -ne 0 ]]; then
+ echo -e "${red}请使用 root 运行脚本${plain}"
+ exit 1
 fi
 
-# 检查操作系统并设置 release 变量
-if [[ -f /etc/os-release ]]; then
-    source /etc/os-release
-    release=$ID
-elif [[ -f /usr/lib/os-release ]]; then
-    source /usr/lib/os-release
-    release=$ID
-else
-    echo "无法检查系统操作系统，请联系作者！" >&2
-    exit 1
-fi
-echo "操作系统版本为: $release"
-
-arch() {
-    case "$(uname -m)" in
-    x86_64 | x64 | amd64) echo 'amd64' ;;
-    i*86 | x86) echo '386' ;;
-    armv8* | armv8 | arm64 | aarch64) echo 'arm64' ;;
-    armv7* | armv7 | arm) echo 'armv7' ;;
-    armv6* | armv6) echo 'armv6' ;;
-    armv5* | armv5) echo 'armv5' ;;
-    s390x) echo 's390x' ;;
-    *) echo -e "${green}不支持的 CPU 架构！ ${plain}" && rm -f install.sh && exit 1 ;;
-    esac
+# detect architecture
+detect_arch() {
+ case "$(uname -m)" in
+ x86_64) echo amd64 ;;
+ aarch64) echo arm64 ;;
+ armv7l) echo armv7 ;;
+ armv6l) echo armv6 ;;
+ *) 
+  echo -e "${red}不支持的CPU架构: $(uname -m)${plain}"
+  exit 1
+ ;;
+ esac
 }
 
-echo "架构: $(arch)"
+ARCH=$(detect_arch)
+echo "检测到架构: $ARCH"
 
-os_version=""
-os_version=$(grep "^VERSION_ID" /etc/os-release | cut -d '=' -f2 | tr -d '"' | tr -d '.')
-
-if [[ "${release}" == "arch" ]]; then
-    echo "您的操作系统是 Arch Linux"
-elif [[ "${release}" == "parch" ]]; then
-    echo "您的操作系统是 Parch Linux"
-elif [[ "${release}" == "manjaro" ]]; then
-    echo "您的操作系统是 Manjaro"
-elif [[ "${release}" == "armbian" ]]; then
-    echo "您的操作系统是 Armbian"
-elif [[ "${release}" == "alpine" ]]; then
-    echo "您的操作系统是 Alpine Linux"
-elif [[ "${release}" == "opensuse-tumbleweed" ]]; then
-    echo "您的操作系统是 OpenSUSE Tumbleweed"
-elif [[ "${release}" == "openEuler" ]]; then
-    if [[ ${os_version} -lt 2203 ]]; then
-        echo -e "${red} 请使用 OpenEuler 22.03 或更高版本 ${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "centos" ]]; then
-    if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red} 请使用 CentOS 8 或更高版本 ${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "ubuntu" ]]; then
-    if [[ ${os_version} -lt 2004 ]]; then
-        echo -e "${red} 请使用 Ubuntu 20 或更高版本！${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "fedora" ]]; then
-    if [[ ${os_version} -lt 36 ]]; then
-        echo -e "${red} 请使用 Fedora 36 或更高版本！${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "amzn" ]]; then
-    if [[ ${os_version} != "2023" ]]; then
-        echo -e "${red} 请使用 Amazon Linux 2023！${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "debian" ]]; then
-    if [[ ${os_version} -lt 11 ]]; then
-        echo -e "${red} 请使用 Debian 11 或更高版本 ${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "almalinux" ]]; then
-    if [[ ${os_version} -lt 80 ]]; then
-        echo -e "${red} 请使用 AlmaLinux 8.0 或更高版本 ${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "rocky" ]]; then
-    if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red} 请使用 Rocky Linux 8 或更高版本 ${plain}\n" && exit 1
-    fi
-elif [[ "${release}" == "ol" ]]; then
-    if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red} 请使用 Oracle Linux 8 或更高版本 ${plain}\n" && exit 1
-    fi
+# detect OS
+if [ -f /etc/os-release ]; then
+ . /etc/os-release
+ OS=$ID
 else
-    echo -e "${red}您的操作系统不受此脚本支持。${plain}\n"
-    echo "请确保您使用的是以下支持的操作系统之一："
-    echo "- Ubuntu 20.04+"
-    echo "- Debian 11+"
-    echo "- CentOS 8+"
-    echo "- OpenEuler 22.03+"
-    echo "- Fedora 36+"
-    echo "- Arch Linux"
-    echo "- Parch Linux"
-    echo "- Manjaro"
-    echo "- Armbian"
-    echo "- AlmaLinux 8.0+"
-    echo "- Rocky Linux 8+"
-    echo "- Oracle Linux 8+"
-    echo "- OpenSUSE Tumbleweed"
-    echo "- Amazon Linux 2023"
-    exit 1
+ echo -e "${red}无法识别操作系统${plain}"
+ exit 1
 fi
+
+echo "检测到系统: $OS"
 
 install_base() {
-    case "${release}" in
-    ubuntu | debian | armbian)
-        apt-get update && apt-get install -y -q wget curl tar tzdata
-        ;;
-    centos | almalinux | rocky | ol)
-        yum -y update && yum install -y -q wget curl tar tzdata
-        ;;
-    fedora | amzn)
-        dnf -y update && dnf install -y -q wget curl tar tzdata
-        ;;
-    arch | manjaro | parch)
-        pacman -Syu && pacman -Syu --noconfirm wget curl tar tzdata
-        ;;
-    opensuse-tumbleweed)
-        zypper refresh && zypper -q install -y wget curl tar timezone
-        ;;
-    *)
-        apt-get update && apt install -y -q wget curl tar tzdata
-        ;;
-    esac
+
+ case "$OS" in
+ ubuntu|debian)
+  apt update
+  apt install -y wget curl tar tzdata
+ ;;
+ centos|rocky|almalinux)
+  yum install -y wget curl tar tzdata
+ ;;
+ fedora)
+  dnf install -y wget curl tar tzdata
+ ;;
+ arch|manjaro)
+  pacman -Syu --noconfirm wget curl tar tzdata
+ ;;
+ *)
+  echo -e "${yellow}未知系统，尝试使用 apt${plain}"
+  apt update
+  apt install -y wget curl tar tzdata
+ ;;
+ esac
+
 }
 
-gen_random_string() {
-    local length="$1"
-    local random_string=$(LC_ALL=C tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w "$length" | head -n 1)
-    echo "$random_string"
+download_xui() {
+
+FILE="x-ui-linux-${ARCH}.tar.gz"
+
+URL1="https://github.com/${REPO}/releases/download/${VERSION}/${FILE}"
+URL2="https://ghproxy.com/${URL1}"
+
+echo "开始下载..."
+
+if wget -O ${FILE} ${URL1}; then
+ echo -e "${green}GitHub 下载成功${plain}"
+else
+ echo -e "${yellow}GitHub 下载失败，尝试加速镜像...${plain}"
+ wget -O ${FILE} ${URL2}
+fi
+
+if [ ! -f ${FILE} ]; then
+ echo -e "${red}下载失败${plain}"
+ exit 1
+fi
+
 }
 
-config_after_install() {
-    local existing_username=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo '用户名: .+' | awk '{print $2}')
-    local existing_password=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo '密码: .+' | awk '{print $2}')
-    local existing_webBasePath=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo '面板路径: .+' | awk '{print $2}')
-    local existing_port=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo '端口: .+' | awk '{print $2}')
-    local server_ip=$(curl -s https://api.ipify.org)
+install_xui() {
 
-    if [[ ${#existing_webBasePath} -lt 4 ]]; then
-        if [[ "$existing_username" == "admin" && "$existing_password" == "admin" ]]; then
-            local config_webBasePath=$(gen_random_string 15)
-            local config_username=$(gen_random_string 10)
-            local config_password=$(gen_random_string 10)
+cd /usr/local
 
-            read -p "您是否要自定义面板端口设置？（如果不自定义，将使用随机端口） [y/n]: " config_confirm
-            if [[ "${config_confirm}" == "y" || "${config_confirm}" == "Y" ]]; then
-                read -p "请设置面板端口: " config_port
-                echo -e "${yellow}您的面板端口为: ${config_port}${plain}"
-            else
-                local config_port=$(shuf -i 1024-62000 -n 1)
-                echo -e "${yellow}生成的随机端口为: ${config_port}${plain}"
-            fi
+if [ -d "$INSTALL_DIR" ]; then
+ systemctl stop x-ui || true
+ rm -rf $INSTALL_DIR
+fi
 
-            /usr/local/x-ui/x-ui setting -username "${config_username}" -password "${config_password}" -port "${config_port}" -webBasePath "${config_webBasePath}"
-            echo -e "这是一个全新安装，出于安全考虑，生成随机登录信息："
-            echo -e "###############################################"
-            echo -e "${green}用户名: ${config_username}${plain}"
-            echo -e "${green}密码: ${config_password}${plain}"
-            echo -e "${green}端口: ${config_port}${plain}"
-            echo -e "${green}路径: ${config_webBasePath}${plain}"
-            echo -e "${green}访问地址: http://${server_ip}:${config_port}/${config_webBasePath}${plain}"
-            echo -e "###############################################"
-            echo -e "${yellow}如果您忘记了登录信息，可以输入 'x-ui settings' 查看${plain}"
-        else
-            local config_webBasePath=$(gen_random_string 15)
-            echo -e "${yellow}路径 缺失或太短，正在生成新的...${plain}"
-            /usr/local/x-ui/x-ui setting -webBasePath "${config_webBasePath}"
-            echo -e "${green}新的 路径: ${config_webBasePath}${plain}"
-            echo -e "${green}访问地址: http://${server_ip}:${existing_port}/${config_webBasePath}${plain}"
-        fi
-    else
-        if [[ "$existing_username" == "admin" && "$existing_password" == "admin" ]]; then
-            local config_username=$(gen_random_string 10)
-            local config_password=$(gen_random_string 10)
+tar zxvf x-ui-linux-${ARCH}.tar.gz
+rm -f x-ui-linux-${ARCH}.tar.gz
 
-            echo -e "${yellow}检测到默认凭据，需要进行安全更新...${plain}"
-            /usr/local/x-ui/x-ui setting -username "${config_username}" -password "${config_password}"
-            echo -e "生成新的随机登录凭据："
-            echo -e "###############################################"
-            echo -e "${green}用户名: ${config_username}${plain}"
-            echo -e "${green}密码: ${config_password}${plain}"
-            echo -e "###############################################"
-            echo -e "${yellow}如果您忘记了登录信息，可以输入 'x-ui settings' 查看${plain}"
-        else
-            echo -e "${green}用户名、密码和 路径 已正确设置。退出...${plain}"
-        fi
-    fi
+cd x-ui
 
-    /usr/local/x-ui/x-ui migrate
+chmod +x x-ui
+chmod +x bin/xray-linux-*
+
+cp x-ui.service /etc/systemd/system/
+
+systemctl daemon-reload
+systemctl enable x-ui
+systemctl restart x-ui
+
 }
 
-install_x-ui() {
-    cd /usr/local/
-
-    if [ $# == 0 ]; then
-        tag_version=$(curl -Ls "https://api.github.com/repos/xmm0722/3x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-        if [[ ! -n "$tag_version" ]]; then
-            echo -e "${red}无法获取 x-ui 版本，请确保您的网络可以访问 GitHub API${plain}"
-            exit 1
-        fi
-        echo -e "获取到 x-ui 最新版本: ${tag_version}，开始安装..."
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch).tar.gz https://github.com/xmm0722/3x-ui/releases/download/${tag_version}/x-ui-linux-$(arch).tar.gz
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 x-ui 失败，请确保您的网络可以访问 GitHub ${plain}"
-            exit 1
-        fi
-    else
-        tag_version=$1
-        tag_version_numeric=${tag_version#v}
-        min_version="2.5.0"
-
-        if [[ "$(printf '%s\n' "$min_version" "$tag_version_numeric" | sort -V | head -n1)" != "$min_version" ]]; then
-            echo -e "${red}请使用更新的版本（至少 v2.5.0）。退出安装。${plain}"
-            exit 1
-        fi
-
-        url="https://github.com/xmm0722/3x-ui/releases/download/${tag_version}/x-ui-linux-$(arch).tar.gz"
-        echo -e "开始安装 x-ui $1"
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch).tar.gz ${url}
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 x-ui $1 失败，请检查版本是否存在 ${plain}"
-            exit 1
-        fi
-    fi
-
-    if [[ -e /usr/local/x-ui/ ]]; then
-        systemctl stop x-ui
-        rm /usr/local/x-ui/ -rf
-    fi
-
-    tar zxvf x-ui-linux-$(arch).tar.gz
-    rm x-ui-linux-$(arch).tar.gz -f
-    cd x-ui
-    chmod +x x-ui
-
-    # 检查系统架构并重命名文件
-    if [[ $(arch) == "armv5" || $(arch) == "armv6" || $(arch) == "armv7" ]]; then
-        mv bin/xray-linux-$(arch) bin/xray-linux-arm
-        chmod +x bin/xray-linux-arm
-    fi
-
-    chmod +x x-ui bin/xray-linux-$(arch)
-    cp -f x-ui.service /etc/systemd/system/
-    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/xmm0722/3x-ui/main/x-ui.sh
-    chmod +x /usr/local/x-ui/x-ui.sh
-    chmod +x /usr/bin/x-ui
-    config_after_install
-
-    systemctl daemon-reload
-    systemctl enable x-ui
-    systemctl start x-ui
-    echo -e "${green}x-ui ${tag_version}${plain} 安装完成，正在运行..."
-    echo -e ""
-    echo -e "
-   ${blue}x-ui 管理菜单（子命令）：${plain}
-                                           
-   ${blue}x-ui${plain}              - 管理脚本
-   ${blue}x-ui start${plain}        - 启动
-   ${blue}x-ui stop${plain}         - 停止
-   ${blue}x-ui restart${plain}      - 重启
-   ${blue}x-ui status${plain}       - 当前状态
-   ${blue}x-ui settings${plain}     - 当前设置
-   ${blue}x-ui enable${plain}       - 启用开机自启动
-   ${blue}x-ui disable${plain}      - 禁用开机自启动
-   ${blue}x-ui log${plain}          - 查看日志
-   ${blue}x-ui banlog${plain}       - 查看封禁日志
-   ${blue}x-ui update${plain}       - 更新
-   ${blue}x-ui legacy${plain}       - 旧版
-   ${blue}x-ui install${plain}      - 安装
-   ${blue}x-ui uninstall${plain}    - 卸载 
-   "
+random_string() {
+ tr -dc A-Za-z0-9 </dev/urandom | head -c $1
 }
 
-echo -e "${green}正在运行...${plain}"
+config_panel() {
+
+USERNAME=$(random_string 8)
+PASSWORD=$(random_string 12)
+PORT=$(shuf -i 10000-60000 -n 1)
+PATH_RANDOM=$(random_string 12)
+
+$INSTALL_DIR/x-ui setting \
+-username $USERNAME \
+-password $PASSWORD \
+-port $PORT \
+-webBasePath $PATH_RANDOM
+
+IP=$(curl -s --max-time 5 https://api.ipify.org || echo "SERVER_IP")
+
+echo ""
+echo "================================"
+echo -e "${green}3x-ui 安装完成${plain}"
+echo ""
+echo "访问地址:"
+echo "http://${IP}:${PORT}/${PATH_RANDOM}"
+echo ""
+echo "用户名: $USERNAME"
+echo "密码: $PASSWORD"
+echo "================================"
+
+}
+
 install_base
-install_x-ui $1
+download_xui
+install_xui
+config_panel
